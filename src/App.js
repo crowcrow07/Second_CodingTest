25fc2a1a3caac9e69c7c29bd335c407ca29dfc69
crowcrow07 <skathd3402@gmail.com>
2024-01-09 19:33:52 +0900

    feat : Order export

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
-import { useRecoilValue } from "recoil";
-import { cartState } from "./recoil/atoms/cartState";
-import Home from "./screen/Home/Home";
 import { Routes, Route } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import Home from "./screen/Home/Home";
+import Order from "./screen/Order";
+
+import "./App.css";
+
+import { cartState } from "./recoil/atoms/cartState";
 
 function App() {
   const cart = useRecoilValue(cartState);
@@ -10,7 +14,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/order" element={<div>order</div>} />
+      <Route path="/order" element={<Order />} />
       <Route path="/complete" element={<div>성공</div>} />
       <Route path="/error" element={<div>에러</div>} />
       <Route path="*" element={<div>유효하지않은 경로</div>} />
