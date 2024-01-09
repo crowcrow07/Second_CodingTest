51389cd6f4444d28629232bebf359ae7f3057eb9
crowcrow07 <skathd3402@gmail.com>
2024-01-09 20:56:04 +0900

    feat : itemCard export

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,13 @@
+import ItemCard from "../itemCard/ItemCard";
+
 import styles from "./List.module.css";
 
 export default function List() {
   return (
     <main className={styles.container}>
-      <div className={styles.scroll}></div>
+      <div className={styles.scroll}>
+        <ItemCard />
+      </div>
     </main>
   );
 }
---
+++ src/components/itemCard/ItemCard.jsx
@@ -0,0 +1,5 @@
+import styles from "./ItemCard.module.css";
+
+export default function ItemCard() {
+  return <div className={styles.container}>카드</div>;
+}
