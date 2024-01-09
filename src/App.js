38963ce46889e6f8bede538dfbc687c3dd668200
crowcrow07 <skathd3402@gmail.com>
2024-01-09 20:36:30 +0900

    feat : max width 768px 고정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useRecoilValue } from "recoil";
 import Home from "./screen/Home/Home";
 import Order from "./screen/Order/Order";
 
-import "./App.css";
+import styles from "./App.module.css";
 
 import { cartState } from "./recoil/atoms/cartState";
 
@@ -12,13 +12,15 @@ function App() {
   const cart = useRecoilValue(cartState);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/order" element={<Order />} />
-      <Route path="/complete" element={<div>성공</div>} />
-      <Route path="/error" element={<div>에러</div>} />
-      <Route path="*" element={<div>유효하지않은 경로</div>} />
-    </Routes>
+    <div className={styles.container}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/complete" element={<div>성공</div>} />
+        <Route path="/error" element={<div>에러</div>} />
+        <Route path="*" element={<div>유효하지않은 경로</div>} />
+      </Routes>
+    </div>
   );
 }
 
