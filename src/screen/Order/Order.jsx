925678dd957a179c0ea25d3ebbc2d6569566b184
crowcrow07 <skathd3402@gmail.com>
2024-01-10 20:36:47 +0900

    feat : useQuery GET 요청

--- src/screen/Order/Order.jsx
+++ src/screen/Order/Order.jsx
@@ -1,15 +1,37 @@
 import Header from "../../components/Header/Header";
 import List from "../../components/List/List";
 import OrderBox from "../../components/OrderBox/OrderBox";
+import Result from "../Result/Result";
+
+import { fetchData } from "../../api/fetchData";
+import { useQuery } from "@tanstack/react-query";
 
 import styles from "./Order.module.css";
 
 export default function Order() {
+  const { isPending, data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchData,
+  });
+
+  const renderContent = () => {
+    if (isPending) {
+      return (
+        <Result
+          url={null}
+          firstRow={"목록을"}
+          secondRow={"불러오고 있습니다."}
+        />
+      );
+    }
+    return <List data={data} />;
+  };
+
   return (
     <section className={styles.container}>
       <Header />
-      <List />
-      <OrderBox />
+      {renderContent()}
+      <OrderBox isPending={isPending} />
     </section>
   );
 }
