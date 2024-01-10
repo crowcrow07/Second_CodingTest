19d3f900045f13d815c0c611a98039c55a8a251b
crowcrow07 <skathd3402@gmail.com>
2024-01-10 20:37:13 +0900

    feat : itemCard Map()

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,14 +2,13 @@ import ItemCard from "../itemCard/ItemCard";
 
 import styles from "./List.module.css";
 
-export default function List() {
+export default function List({ data }) {
   return (
     <main className={styles.container}>
       <div className={styles.scroll}>
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+        {data.map((item) => {
+          return <ItemCard key={item.id} item={item} />;
+        })}
       </div>
     </main>
   );
