4e969366ac8a1d80dfa249325a1843cc6c6fd428
crowcrow07 <skathd3402@gmail.com>
2024-01-10 20:06:50 +0900

    feat : result 페이지 퍼블리싱

--- src/screen/Result/Result.jsx
+++ src/screen/Result/Result.jsx
@@ -1,3 +1,11 @@
 import styles from "./Result.module.css";
 
-export default function Result() {}
+export default function Result({ url = null, firstRow = "", secondRow = "" }) {
+  return (
+    <div className={styles.container}>
+      <img className={!url && styles.none} src={url} alt="icon" />
+      <p>{firstRow}</p>
+      <p>{secondRow}</p>
+    </div>
+  );
+}
