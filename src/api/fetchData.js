5bde1ba0e180ce58104f0dc36508c308047554eb
crowcrow07 <skathd3402@gmail.com>
2024-01-10 20:25:39 +0900

    feat : fetchData

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,1 +1,7 @@
-export const fetchData = () => {};
+const API_KEY = "http://localhost:3001";
+
+export const fetchData = async () => {
+  const response = await fetch(`${API_KEY}/items`);
+  const data = await response.json();
+  return data;
+};
