20e006f17d19c5fa4b6761bae9ed95ab66624d8c
crowcrow07 <skathd3402@gmail.com>
2024-01-10 20:38:00 +0900

    feat : 데이터 화면에 배치

--- src/components/itemCard/ItemCard.jsx
+++ src/components/itemCard/ItemCard.jsx
@@ -1,21 +1,23 @@
 import styles from "./ItemCard.module.css";
 
-export default function ItemCard(props) {
+export default function ItemCard({ item }) {
+  const { name, event, price } = item;
+
   return (
     <div className={`${styles.container}`}>
       <div className={styles.itemImage} />
       <div className={styles.itemInfo}>
         <div className={styles.title}>
-          <div>제목</div>
-          <div>이벤트</div>
+          <div>{name}</div>
+          <div className={!Boolean(event) && styles.none}>이벤트</div>
         </div>
         <div className={styles.remote}>
           <div>
             <button>-</button>
-            <div>999</div>
+            <div>0</div>
             <button>+</button>
           </div>
-          <div>100,000원</div>
+          <div>{price}원</div>
         </div>
       </div>
     </div>
