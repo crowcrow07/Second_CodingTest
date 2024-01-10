509824170aacb4e6aa1c66c2bc450e16f390984b
crowcrow07 <skathd3402@gmail.com>
2024-01-10 23:22:37 +0900

    feat : postData 구현

--- src/api/postData.js
+++ src/api/postData.js
@@ -1,3 +1,14 @@
 const API_KEY = "http://localhost:3001";
 
-export const postData = () => {};
+export const postData = async (payload) => {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  const response = await fetch(`${API_KEY}/order`, requestOptions);
+  const data = response.json();
+  return data;
+};
